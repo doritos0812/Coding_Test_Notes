f236ab49cf06ab8a5833db4aafd629f761e2e4cb
doritos0812 <dorito2498@gmail.com>
2020-12-24 18:47:18 +0900

    Add Sieve_of_Eratosthenes

---
+++ Numbers/Sieve_of_Eratosthenes.py
@@ -0,0 +1,15 @@
+def Eratosthenes(n):
+    array = [True]*(n+1)
+    for i in range(2,int(n**(1/2))+1):
+        if array[i] == True:
+            j = 2
+            while i * j <= n:
+                array[i * j] = False
+                j += 1
+    for i in range(2, n+1):
+        if array[i]:
+            print(i)             # Show Prime Numbers
+            print(array[2:].count(True))  # Count of Prime Numbers
+
+
+Eratosthenes(10)
