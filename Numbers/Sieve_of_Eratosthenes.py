a6876b88707861582bc53add396d671013de2eb7
doritos0812 <dorito2498@gmail.com>
2020-12-24 18:49:40 +0900

    update examples

--- Numbers/Sieve_of_Eratosthenes.py
+++ Numbers/Sieve_of_Eratosthenes.py
@@ -8,8 +8,11 @@ def Eratosthenes(n):
                 j += 1
     for i in range(2, n+1):
         if array[i]:
-            print(i)             # Show Prime Numbers
+            print(i)                      # Show Prime Numbers
             print(array[2:].count(True))  # Count of Prime Numbers
 
-
+'''
 Eratosthenes(10)
+2 3 5 7 
+4
+'''
