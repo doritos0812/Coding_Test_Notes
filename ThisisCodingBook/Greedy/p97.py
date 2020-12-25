0547ed4cd6a9fa145443e600ddb6305afb2a5618
doritos0812 <dorito2498@gmail.com>
2020-12-25 21:36:05 +0900

    Add p97

---
+++ ThisisCodingBook/Greedy/p97.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+data = []
+small = []
+for i in range(n):
+    a = list(map(int, input().split()))
+    data.append(a)
+    small.append(min(a))
+
+print(small.index(max(small))+1)
+
+
+'''
+
+3 3
+3 1 2 
+4 1 4
+2 2 2
+3
+
+2 4
+7 3 1 8
+3 3 3 4
+2
+'''
